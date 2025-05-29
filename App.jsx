06c1b0d49d3441afbb9b7a4f54e14de71ef5e60b
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-29 15:58:37 +0500

    Added Home and Details Screen

--- App.jsx
+++ App.jsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/components/Home';
+import Details from './src/components/Details';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}></Stack.Screen>
       <Stack.Screen
+        name="Details"
+        component={Details}
+        options={{headerShown: false}}></Stack.Screen>
     </NavigationContainer>
   );
 }
