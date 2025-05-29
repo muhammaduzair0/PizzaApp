fc73e6a71af5259bad3c7fc863b905d66a51dc0b
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-29 15:59:01 +0500

    Added Colors theme In Assets

---
+++ src/assets/colors/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    background: '#f9f9f8',
+    textDark: '#313234',
+    primary: '#F5CA48',
+    secondary: '#F26C68',
+    textLight: '#CDCDCD',
+    price: '#E4723C',
+    white: '#FFF',
+    black: '#000'
+}
