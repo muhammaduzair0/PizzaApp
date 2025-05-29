3b194af05452916004452c9bde5e4d7d525ae1da
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-29 15:58:21 +0500

    Added Stack Navigator Stack Screen

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Screen
+      <Stack.Screen
+    </NavigationContainer>
+  );
+}
