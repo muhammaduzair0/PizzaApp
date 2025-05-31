271c67bcf8ed8813e184fa772ac282832d701fe7
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-05-31 16:12:11 +0500

    Home Screen Styling added

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -147,3 +147,186 @@ const Home = ({navigation}) => {
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    alignItems: 'center',
+  },
+  profileImg: {
+    height: 40,
+    width: 40,
+    borderRadius: 40,
+  },
+  titlesWrapper: {
+    marginTop: 30,
+    paddingHorizontal: 20,
+  },
+  foodTitle: {
+    fontSize: 16,
+    fontFamily: 'Montserrat-Bold',
+    color: colors.textDark,
+  },
+  deliveryTitle: {
+    fontSize: 32,
+    fontFamily: 'Montserrat-Bold',
+    color: colors.textDark,
+    marginTop: 5,
+  },
+  searchWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    marginTop: 40,
+  },
+  search: {
+    flex: 1,
+    marginLeft: 10,
+    borderBottomColor: colors.textLight,
+    borderBottomWidth: 2,
+  },
+  searchText: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 14,
+    marginBottom: 5,
+    color: colors.textLight,
+  },
+  categoriesWrapper: {
+    marginTop: 30,
+  },
+  categoriesTitle: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 16,
+    paddingHorizontal: 20,
+    color: colors.textDark,
+  },
+  categoriesListWrapper: {
+    paddingTop: 15,
+    paddingBottom: 20,
+  },
+  categoryItemWrapper: {
+    backgroundColor: '#F5CA48',
+    marginRight: 20,
+    borderRadius: 20,
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 10,
+    elevation: 2,
+  },
+  categoryItemImg: {
+    width: 60,
+    height: 60,
+    marginTop: 25,
+    alignSelf: 'center',
+    marginHorizontal: 20,
+  },
+  categoryItemTitle: {
+    textAlign: 'center',
+    fontFamily: 'Montserrat-Medium',
+    fontSize: 14,
+    marginTop: 10,
+  },
+  categorySelectWrapper: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    width: 26,
+    height: 26,
+    borderRadius: 26,
+    marginBottom: 20,
+  },
+  categorySelectIcon: {
+    alignSelf: 'center',
+  },
+  popularWrapper: {
+    paddingHorizontal: 20,
+  },
+  popularTitle: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 16,
+    color: colors.textDark,
+  },
+  popularCardWrapper: {
+    backgroundColor: colors.white,
+    borderRadius: 25,
+    paddingTop: 20,
+    paddingLeft: 20,
+    flexDirection: 'row',
+    overflow: 'hidden',
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 10,
+    elevation: 2,
+  },
+  popularTopWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  popularTopText: {
+    marginLeft: 10,
+    fontFamily: 'Montserrat-SemiBold',
+    color: colors.textDark,
+    fontSize: 14,
+  },
+  popularTitlesWrapper: {
+    marginTop: 20,
+  },
+  popularTitlesTitle: {
+    fontFamily: 'Montserrat-SemiBold',
+    color: colors.textDark,
+    fontSize: 14,
+  },
+  popularTitlesWeight: {
+    fontFamily: 'Montserrat-Medium',
+    color: colors.textLight,
+    fontSize: 12,
+    marginTop: 5,
+  },
+  popularCardBottom: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+    marginLeft: -20,
+  },
+  addPizzaButton: {
+    backgroundColor: colors.primary,
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 25,
+  },
+  ratingWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 20,
+  },
+  rating: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 12,
+    color: colors.textDark,
+    marginLeft: 5,
+  },
+  popularCardRight: {
+    marginLeft: 40,
+  },
+  popularCardImage: {
+    width: 210,
+    height: 125,
+    resizeMode: 'contain',
+  },
+});
+
+export default Home;
